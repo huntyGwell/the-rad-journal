64b4869883a04af2d65ef46e04b7121a458fb452
huntyGwell <huntergruwell@gmail.com>
2024-04-24 13:57:46 -0600

    add cpp nad header files

---
+++ journal.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream> // 
+#include <memory> //
+
+#include "ascii-art.h"
+#include "ice-breaker-prompts.h"
+
+int main(){
+    
+}
