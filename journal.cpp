36101d1c3029d160139e62a9060c2223928b842e
huntyGwell <huntergruwell@gmail.com>
2024-04-24 22:49:06 -0600

    nerfed for a test

--- journal.cpp
+++ journal.cpp
@@ -4,8 +4,8 @@
 #include <fstream> // 
 #include <memory> //
 
-#include "ascii-art.h"
-#include "ice-breaker-prompts.h"
+//#include "ascii-art.h"
+//#include "ice-breaker-prompts.h"
 
 int main(){
     std::cout << "Welcome to your journal!" << std::endl;
